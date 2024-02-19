c60f3e71501d68ad7ba833274425c15621cf3309
Ziklson <96010534+Ziklson@users.noreply.github.com>
2024-02-19 13:25:19 +0300

    Add playground.swift file with code

---
+++ playground.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let firstName="Aleksey"
+let lastName="Kuvarin"
+
+let age = 21
+
+let weight=65.6
+let isMarried=false
+
+print("first name: \(firstName) \nlast name: \(lastName) \nage: \(age) \nweight: \(weight) \nisMarried? - \(isMarried)")
+
